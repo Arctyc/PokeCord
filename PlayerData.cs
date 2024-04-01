f70ce638cb6ffe834b293e2690c94da4dc9b2069
Arctyc <62357807+Arctyc@users.noreply.github.com>
2024-04-01 11:49:46 -0400

    fix for missing shit from weird github stuff

--- PlayerData.cs
+++ PlayerData.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace PokeCord
 {
-    internal class PlayerData
+    public class PlayerData
     {
-        public int version = 1;
+        public int Version { get; set; } = 2;
         public ulong UserId {  get; set; }
         public string UserName { get; set; }
         public int Experience { get; set; }
         public int Pokeballs { get; set; }
         public List<PokemonData> CaughtPokemon {  get; set; }
+        public Dictionary<Badge, DateTime> Badges { get; set; } = new Dictionary<Badge, DateTime>();
         public PlayerData()
         {
             CaughtPokemon = new List<PokemonData>();
