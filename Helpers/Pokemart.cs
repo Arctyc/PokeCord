6e73559de5656edc09237fb3b1f23de49e2f03a4
Arctyc <62357807+Arctyc@users.noreply.github.com>
2024-04-23 22:47:18 -0400

    Increased amulet coin cost from 250 to 500

--- Helpers/Pokemart.cs
+++ Helpers/Pokemart.cs
@@ -17,7 +17,7 @@ namespace PokeCord.Helpers
         // Poke Mart Menu
         public const int CostPokeballs = 500;
         public const int AmountPokeballs = 10;
-        public const int CostAmuletCoin = 250;
+        public const int CostAmuletCoin = 500; // Was 250
         public const int AmountAmuletCoin = 50;
         public const int CostExpShare = 1500;
         public const int AmountExpShare = 10;
