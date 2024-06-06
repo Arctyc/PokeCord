3d5108866fb61ac50de4b562457dd2d4a22d4a44
Arctyc <62357807+Arctyc@users.noreply.github.com>
2024-06-06 15:14:13 -0400

    refactored for modularity in pokeselector.

--- Helpers/PokeSelector.cs
+++ Helpers/PokeSelector.cs
@@ -6,88 +6,71 @@ namespace PokeCord.Helpers
 {
     public class PokeSelector
     {
-        //TODO: Collect a range of alternate forms (10001-10277) that have artwork, give them a chance to be caught
-
-        private readonly int _minPokemonIdSpecial= 10001;
+        // Highest and lowest Pokemon ID for special Pokemon
+        private readonly int _minPokemonIdSpecial = 10001;
         private readonly int _maxPokemonIdSpecial = 10277;
 
-        //private readonly double _upperRangeProbability = 0.05; // 5% chance of selecting alternate form
+        // Highest Pokemon ID to be requested on PokeApi
+        private readonly int _maxPokemonId = 1025;
 
-        private readonly int _maxPokemonId = 1025; // Highest Pokemon ID to be requested on PokeApi
-        private readonly int _shinyRatio = 256; // Chance of catching a shiny
-        private readonly int _charmShinyRatio = 128; // Chance of catching a shiny with shiny charm item
+        // Chance of catching a shiny
+        private readonly int _shinyRatio = 256;
 
-        private const int shinyExpMultiplier = 4; // Amount to multiply base exp by if shiny
+        // Chance of catching a shiny with shiny charm item
+        private readonly int _charmShinyRatio = 128;
 
-        private int defaultExperience = 150; //new Random().Next(125, 176); // exp to be used in the case that there is no base exp provided
+        // Amount to multiply base exp by if shiny
+        private const int shinyExpMultiplier = 4;
+
+        // Default experience to be used in the case that there is no base exp provided
+        private const int defaultExperience = 150;
 
         public PokeSelector()
         {
         }
 
+        // Get a random Pokemon
         public async Task<PokemonData> GetRandomPokemon(PokeApiClient pokeApiClient, PlayerData playerData)
         {
-            int playerShinyRatio = _shinyRatio;
-            // Check for shiny charm
-            if (CheckPlayerShinyCharm(playerData))
-            {
-                playerShinyRatio = _charmShinyRatio;
-            }
-            //CSPRNG Random
-            int randomId = RandomNumberGenerator.GetInt32(1, _maxPokemonId + 1);
-            int shinyCheck = RandomNumberGenerator.GetInt32(1, playerShinyRatio + 1);
-
-            bool shiny = shinyCheck == playerShinyRatio;
-            Console.WriteLine($"PokeSelector Values - PokemonID: {randomId}, Shiny Roll: {shinyCheck}/{playerShinyRatio}");
-
-            Pokemon pokemon = await pokeApiClient.GetResourceAsync<Pokemon>(randomId);
-
-            if (pokemon != null)
-            {
-                // Assign experience and avoid null with default.
-                int experience = pokemon.BaseExperience ?? defaultExperience;
-                pokemon.Name = char.ToUpper(pokemon.Name[0]) + pokemon.Name.Substring(1);
-
-                string? imageUrl;
-                if (!shiny)
-                {
-                    // Assign default image
-                    imageUrl = pokemon.Sprites.Other.OfficialArtwork.FrontDefault;
-                }
-                else
-                {
-                    // Assign shiny image
-                    imageUrl = pokemon.Sprites.Other.OfficialArtwork.FrontShiny;
-                    experience *= shinyExpMultiplier; // Add shiny exp
-                }
-                return new PokemonData
-                {
-                    PokedexId = pokemon.Id,
-                    Name = pokemon.Name,
-                    ImageUrl = imageUrl,
-                    BaseExperience = experience,
-                    Timestamp = DateTime.UtcNow,
-                    Shiny = shiny
-                };
-            }
-            else
-            {
-                Console.WriteLine($"Error fetching data for Pokemon ID: {randomId}");
-                return null;
-            }
+            int playerShinyRatio = GetPlayerShinyRatio(playerData);
+            int randomId = GetRandomId(1, _maxPokemonId + 1);
+            return await GetPokemon(pokeApiClient, randomId, playerShinyRatio);
         }
 
+        // Get an event Pokemon
         public async Task<PokemonData> GetEventPokemon(PokeApiClient pokeApiClient, PlayerData playerData)
         {
-            int playerShinyRatio = _shinyRatio;
-            //CSPRNG Random
-            int randomId = RandomNumberGenerator.GetInt32(_minPokemonIdSpecial, _maxPokemonIdSpecial + 1);
-            int shinyCheck = RandomNumberGenerator.GetInt32(1, playerShinyRatio + 1);
+            int playerShinyRatio = GetPlayerShinyRatio(playerData);
+            int randomId = GetRandomId(_minPokemonIdSpecial, _maxPokemonIdSpecial + 1);
+            return await GetPokemon(pokeApiClient, randomId, playerShinyRatio);
+        }
 
-            bool shiny = shinyCheck == playerShinyRatio;
-            Console.WriteLine($"*EGG* PokeSelector Values - PokemonID: {randomId}, Shiny Roll: {shinyCheck}/{playerShinyRatio}");
+        // Get the shiny ratio for a player
+        private int GetPlayerShinyRatio(PlayerData playerData)
+        {
+            return CheckPlayerShinyCharm(playerData) ? _charmShinyRatio : _shinyRatio;
+        }
 
-            Pokemon pokemon = await pokeApiClient.GetResourceAsync<Pokemon>(randomId);
+        // Check if a player has a shiny charm
+        private bool CheckPlayerShinyCharm(PlayerData playerData)
+        {
+            return playerData.PokeMartItems.TryGetValue("Shiny Charm", out _);
+        }
+
+        // Get a random Pokemon ID
+        private int GetRandomId(int min, int max)
+        {
+            return RandomNumberGenerator.GetInt32(min, max);
+        }
+
+        // Get a Pokemon
+        private async Task<PokemonData> GetPokemon(PokeApiClient pokeApiClient, int pokemonId, int shinyRatio)
+        {
+            int shinyCheck = RandomNumberGenerator.GetInt32(1, shinyRatio + 1);
+            bool shiny = shinyCheck == shinyRatio;
+            Console.WriteLine($"PokeSelector Values - PokemonID: {pokemonId}, Shiny Roll: {shinyCheck}/{shinyRatio}");
+
+            Pokemon pokemon = await pokeApiClient.GetResourceAsync<Pokemon>(pokemonId);
 
             if (pokemon != null)
             {
@@ -124,22 +107,14 @@ namespace PokeCord.Helpers
             }
             else
             {
-                Console.WriteLine($"Error fetching data for Pokemon ID: {randomId}");
+                Console.WriteLine($"Error fetching data for Pokemon ID: {pokemonId}");
                 return null;
             }
         }
-
-        private bool CheckPlayerShinyCharm(PlayerData playerData)
-        {
-            if (playerData.PokeMartItems.TryGetValue("Shiny Charm", out int playerShinyCharm))
-            {
-                return true;
-            }
-            return false;
-        }
     }
 
-    public class PokemonData // Simple data structure for Pokemon information
+    // Simple data structure for Pokemon information
+    public class PokemonData
     {
         public int PokedexId { get; set; }
         public bool Shiny { get; set; }
